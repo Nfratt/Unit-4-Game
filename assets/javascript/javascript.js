93563a51b9600e86f1050ef9041d951f6ad01c79
Nick Frattaroli <Nafratt@gmail.com>
2019-09-04 11:25:36 -0400

    updating js

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,48 +1,101 @@
 var wins =0;
 var losses = 0;
-var gemcount =0;
+var score =0;
 var computerGuess = getRandomInt
 var gem1 =Math.floor(Math.random()*13)
 var gem2 =Math.floor(Math.random()*13)
 var gem3 =Math.floor(Math.random()*13)
 var gem4 =Math.floor(Math.random()*13)
+$( '#reachme').html(computerGuess);
 
-
+function wincheck(){
+    if (score > computerGuess){
+        losses ++;
+        alert('You went to high, you lose');
+        update();
+    }
+    
+    //  else {
+    //     wins++;
+    //     alert('you have won'+wins+'times');
+    //    update();}442
+    };
 function getRandomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (121- 20)) + 20; //The maximum is exclusive and the minimum is inclusive
   }
+  function crystalvalues(){
+     $( '#gem1').html(gem1);
+     $( '#gem2').html(gem2);
+     $( '#gem3').html(gem3);
+     $( '#gem4').html(gem4);}
 function update(){
    
     $( "#wins").html(wins);
     $( '#losses').html(losses);
-    $( '#reachme').html(computerGuess);
-    $( '#gem1').html(gem1);
-    $( '#gem2').html(gem2);
-    $( '#gem3').html(gem3)
-    $( '#gem4').html(gem4)
-    $( '#gemcounter').html(gemcount)
+    // $( '#gem1').html(gem1);
+    // $( '#gem2').html(gem2);
+    // $( '#gem3').html(gem3);
+    // $( '#gem4').html(gem4);
+    // $( '#gemcounter').html(score);
 }
 function checkclick(){
     $('#gem1').on('click',function() {
-        alert('crystal 1'+""+gem1);
-        gemcount += gem1;
+        alert('crystal 1: '+""+gem1);
+        score += gem1;
+        $( '#gemcounter').html(score);
         update();
+        wincheck();
+       
+    })
+
+    $('#gem2').on('click',function() {
+        alert('crystal 2: '+""+gem2);
+        score += gem2;
+        $( '#gemcounter').html(score);
+        update();
+        wincheck()
+    })
+
+    $('#gem3').on('click',function() {
+        alert('crystal 3: '+""+gem3);
+        $( '#gemcounter').html(score);
+        score += gem3;
+        update(); 
+        wincheck()
     
-    }
-    )}
+    })
+    $('#gem4').on('click',function() {
+        alert('crystal 4: '+""+gem4);
+        $( '#gemcounter').html(score);
+        score += gem4;
+        update();
+        wincheck()
     
-if(gemcount== computerGuess){
-    wins++;
-    alert('you have won'+wins+'times');
-    update();
+    })
+
 }
-else if (gemcount>computerGuess){
-    losses ++;
-    alert('You went to high, you lose');
-    update();}
-    
+// if(score == computerGuess){
+//     wins++;
+//     alert('you have won'+wins+'times');
+//     update();
+// // }
+// function wincheck(){
+// if (score > computerGuess){
+//     losses ++;
+//     alert('You went to high, you lose');
+//     update();
+// }
+
+//  else {
+//     wins++;
+//     alert('you have won'+wins+'times');
+//    update();
+// };}
+//  (score == computerGuess)
+ 
+checkclick();
 
 update();
-
+crystalvalues();
